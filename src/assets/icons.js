e0eeaf8974ae75b28dfa385931dfc9f3b4a9bfc8
nitinsomu <nitin.somu13@gmail.com>
2024-11-12 22:20:58 +0530

    Change footer

---
+++ src/assets/icons.js
@@ -0,0 +1,5 @@
+const icons = {
+    "Linkedin" : "https://icon.icepanel.io/Technology/svg/LinkedIn.svg",
+    "Github" : "https://img.icons8.com/ios11/512/FFFFFF/github.png"
+}
+export default icons;
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import icons from "../assets/icons";
 
 function Footer() {
     let year = new Date().getFullYear();
     return (
         <div className="footer-container contact">
-            <section id = "contact">
-            <footer>
-                <div>nitin.somu13@gmail.com</div>
-                <div>{year}</div>
-            </footer>
+            <section id="contact">
+                <footer>
+                    <div>nitin.somu13@gmail.com</div>
+                    <div>{year}</div>
+                    <div>
+                        <a href="https://github.com/nitinsomu" target="_blank" rel="noopener noreferrer">
+                                <img className="icon" src={icons.Github} alt="GitHub" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://linkedin.com/in/nitinsomu" target="_blank" rel="noopener noreferrer">
+                                <img className="icon" src={icons.Linkedin} alt="LinkedIn" />
+                        </a>
+                    </div>
+                </footer>
             </section>
         </div>
     );

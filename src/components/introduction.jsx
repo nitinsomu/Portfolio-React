094555e31312266dd18b4f69f2bb3eb2a4c8054b
nitinsomu <nitin.somu13@gmail.com>
2024-11-12 22:43:13 +0530

    Changed introduction

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -6,7 +6,7 @@ function Introduction() {
       <div>
           <h1 className = "typewriter">Hello, I'm Nitin Somu</h1>
           <p className = "info">
-              I'm a Computer Science Engineer with a strong background in data structures, algorithms, and programming. I have experience in C++ and Python, focusing on backend development and machine learning. I am passionate about exploring new technologies and contributing to innovative projects.
+              I'm a Computer Science Engineer with a strong background in data structures, algorithms, and programming. I have experience in C++ and Python, focusing on backend development and machine learning and cloud computing. I am passionate about exploring new technologies and contributing to innovative projects.
           </p>
           <a href="#projects" class="btn">View My Projects</a>
       </div>

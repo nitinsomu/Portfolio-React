aa4551465ca96c1e8a326a82ac82f17530472869
nitinsomu <nitin.somu13@gmail.com>
2025-05-25 03:39:56 +0530

    Add new experience

--- src/components/exp.jsx
+++ src/components/exp.jsx
@@ -7,8 +7,9 @@ function Exp() {
             <section id="exp">
             <h3 className="title">Experience</h3>
             <div className="card-container">
+                <Card name = "Quality Assurance Intern" desc = "Bengaluru" src = "https://upload.wikimedia.org/wikipedia/commons/7/76/NASDAQ_logo.svg" duration = "January 2025 - Current" srcs = "https://www.nasdaq.com/"/>
                 <Card name = "Technology Summer Intern" desc = "Pune" src = "https://content.influencemap.org//site/data/001/361/1361555.png" duration = "June - July 2024" srcs = "https://home.barclays/"/>
-                <Card name = "Project Intern" desc = "Bengaluru" src = "https://1000logos.net/wp-content/uploads/2016/10/Bosch-Logo.png" duration = "August - September 2024" srcs = "https://www.bosch.in/"/>
+                <Card name = "Project Intern" desc = "Bengaluru" src = "https://1000logos.net/wp-content/uploads/2016/10/Bosch-Logo.png" duration = "August - September 2023" srcs = "https://www.bosch.in/"/>
             </div>
             </section>
         </div>
